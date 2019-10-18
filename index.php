b43c4d19003dd306893828d7b0a33763b03512f4
root <brandon92@hotmail.sg>
2019-10-18 22:27:44 +0800

    commit3

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if(isset($_POST['submit'])) {
 
 					// The HTML-formatted body of the email
 					$bodyHtml = '<h1>Life Long Income</h1>'
-						'<p> First Name:'echo $_POST['fname'];'</p>
+						'<p> First Name:'echo $_POST['fname'];'</p>'
 					//	'<p> Family Name:'$_POST['famname']'</p>'
 					//	'<p> Age Group:'$_POST['agegroup']'</p>'
 					//	'<p> Contact Number:'$_POST['tel']'</p>'
@@ -99,7 +99,7 @@ if(isset($_POST['submit'])) {
 					//	'<p> Desired Passive Income:'$_POST['income']'</p>'
 					//	'<p> Monthly Savings:'$_POST['saving']'</p>'
 					//	'<p> Referral:'$_POST['referral']'</p>'
-						<p>This email was sent through the
+						'<p>This email was sent through the
 						<a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
 						interface using the <a href="https://github.com/PHPMailer/PHPMailer">
 						PHPMailer</a> class.</p>';
